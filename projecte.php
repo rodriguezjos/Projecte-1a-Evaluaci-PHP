3f9edd5f0aaf37b21e345be66d565baf490215a7
rodriguezjos <rodriguezzjose1904@gmail.com>
2023-11-23 16:43:52 +0100

    Css

--- projecte.php
+++ projecte.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     
@@ -11,6 +12,6 @@
 </html>
 
 <?php
-    echo "Hola jose";
-    echo "Hola nicolau";
+
+
 ?>
