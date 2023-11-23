3c127cc6926db3a72b848afe1d6e686bdde12a85
NicolauSegui <nsegui87@alumnes.politecnicllevant.cat>
2023-11-23 17:03:40 +0100

    bottons

--- projecte.php
+++ projecte.php
@@ -1,17 +1,30 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    
+    <form method="post">
+        <input type="submit" name="funcionalitat_1" value="funcionalitat_1" />
+        <input type="submit" name="funcionalitat_2" value="funcionalitat_2" />
+    </form>
 </body>
+
 </html>
 
 <?php
     require('funcions.php');
-    mostra_videojocs(carrega_fitxer());
+      
+    if(isset($_POST['funcionalitat_1'])) { 
+        mostra_videojocs(carrega_fitxer());
+    } 
+    if(isset($_POST['funcionalitat_2'])) { 
+        echo "hola";
+    } 
+
 ?>
